8f7c0405cb59bac4b9ce76cf97dc843accbac9bc
Aditya-jaiswal07972 <104018992+Aditya-jaiswal07972@users.noreply.github.com>
2024-04-29 13:47:27 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -5,14 +5,14 @@ pipeline {
         stage('Clone Repository') {
             steps {
                 // Clone the repository containing your Python script
-                git branch: 'master', credentialsId: 'your-credentials-id', url: 'https://github.com/Aditya-jaiswal07972/Project.git'
+                git branch: 'main', credentialsId: 'jenkin1', url: 'https://github.com/Aditya-jaiswal07972/Project.git'
             }
         }
         
         stage('Build and Run Python Script') {
             steps {
                 // Navigate to the directory containing your Python script
-                dir('path/to/your/python/script') {
+                dir('blob/main/python-captha.py') {
                     // Install any required dependencies using pip
                     sh 'pip install -r requirements.txt'
                     
